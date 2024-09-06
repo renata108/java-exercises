0601b1b5971a2d96afd20b535de9fb6388ec98c9
Renata Biscola <renatabiscola@gmail.com>
2024-09-06 18:42:12 -0300

    Adiciona o arquivo Tabuada.java com a lógica de multiplicação

---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
+    }
+}
